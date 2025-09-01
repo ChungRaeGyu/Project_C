8fa65c1acd528faae9cf54d412c7e0969203d6a9
Chung Rae Gyu <wogns9769@gmail.com>
2025-09-01 21:01:02 +0900

    Deck
    Add : SCVReader
    Add : DeckPanel

--- Assets/Scripts/Manager/LobbyManager.cs
+++ Assets/Scripts/Manager/LobbyManager.cs
@@ -4,6 +4,13 @@ using UnityEngine.UI;
 public class LobbyManager : MonoBehaviour
 {
     public Button quickMatchBtn;
+    [Header("CardPanel")]
+    public Button cardCheckBtn;
+    public Button closeCardPanel;
+    public GameObject cardPanel;
+    [Header("DescriptionPanel")]
+    public GameObject descriptionPanel;
+    public Button closeDescriptionPanel;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +18,20 @@ public class LobbyManager : MonoBehaviour
         {
             NetworkManager.Instance.QuickMatchBtn();
         });
+        cardCheckBtn.onClick.AddListener(OpenCardBoard);
+        closeCardPanel.onClick.AddListener(OpenCardBoard);
+        closeDescriptionPanel.onClick.AddListener(OpenDescriptionPanel);
+    }
+
+    private void OpenCardBoard()
+    {
+        cardPanel.SetActive(!cardPanel.activeSelf);
+    }
+    private void OpenDescriptionPanel()
+    {
+        //카드를 클릭했을때 열리도록 만들면 된다.
+        //카드의 기본정보를 넣고
+        descriptionPanel.SetActive(!descriptionPanel.activeSelf);
     }
 
 }
