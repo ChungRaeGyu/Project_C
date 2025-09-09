00106a19775dabd2dbbb8bd2028c1ea6eb56be7b
insuha <impmiay3567@g.yju.ac.kr>
2025-09-09 11:23:45 +0900

    Update:GameManager.cs
    포톤뷰 추가
    라인별 몬스터공간
    시간관련 변수
    라인변수
    승리조건 변수 추가

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,51 +1,148 @@
 using Photon.Pun;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
+//TODO : 
+/* 점령시 그 쪽 라인 닫아주기(설치불가능 및 해당 라인에 몬스터 삭제)
+ * GhostObject.cs 쪽에 설치불가능한 조건 추가
+ * 각 라인 관리
+ * 점령에 대한 정보는 몬스터나 탑에서 한다.
+ * 
+ * 몬스터를 소환할때 
+ * 1. 탑에 몬스터가 닿였을때 GameManager에 있는 점령을 호출한다.
+ * 2. 점령상태는 공유하돼 점령했다는 자체는 로컬로 관리해도 되겠지?
+ * 3. 점령상태 : 라인 닫기, 라인 닫을 껀데 이건 네트워크 공유 PhotonView를 그럼 GameManager에서 호출 할 필요가 있긴 하네
+ * 4. 점령을 2개 완료하면 네트워크에다가 다 공유 게임이 끝났음을 알림
+ */
+
+//할일  : 라인 만들어서 currentLine 입력시켜 주기, 탑 만들어서 몬스터가 닿았을 때 점령 완료 호출
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     [Header("Test Settings")]
     public Button GoToLobbyBtn;
     public Button GoToRoomBtn;
-    
+
     private Queue<Unit> currentDeck; //이거에서 하나씩 빼서 쓰면 됨
     private List<Unit> currentHands = new List<Unit>(); //현재 손에 들고 있는 카드들
 
     private int startHandCount = 5; //시작할 때 들고 있는 카드 수
-    [SerializeField]private Transform handParent; //손에 들고 있는 카드들 부모
+    [SerializeField] private Transform handParent; //손에 들고 있는 카드들 부모
+
+    //인수
+    private float time = 0;
+    private bool isStart = false;
+
+    public int cost = 0;
+    private float costTime = 0;
+    private int occupation = 3; //점령확인
+
+    private PhotonView pv;
+
+    public List<GameObject>[] gameObjects = new List<GameObject>[3];
+    public bool[] lineBool = new bool[]{ true, true, true };
+
+    public int currentLine = -1; //라인 각 라인에서 호출해줄 꺼다. 각라인에서 호출하기만 하면 된다. 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     private void Awake()
     {
+
         if (Instance != null) { Destroy(gameObject); return; }
         Instance = this;
-        DontDestroyOnLoad(gameObject);
+
+        pv = GetComponent<PhotonView>();
     }
-    void Start()
+    private void Update()
     {
-        GoToLobbyBtn.onClick.AddListener(() =>
+        if (!isStart) return;
+        if(PhotonNetwork.CurrentRoom.CustomProperties.TryGetValue("StartTime",out var t))
         {
-            NetworkManager.Instance.GotoLobby();
-        });
-        GoToRoomBtn.onClick.AddListener(() =>
+            time = PhotonNetwork.ServerTimestamp - (float)t; //시간 동기화
+        }
+        if (time - costTime >= 1)
         {
-            NetworkManager.Instance.ReJoindRoom(PhotonNetwork.CurrentRoom.Name);
-        });
+            costTime = time;
+            cost++;
+            cost = Math.Min(cost, 10);
+        }
 
-        currentDeck = new Queue<Unit>(DataManager.Instance.SuffleDeck());
-        Init();
     }
 
+    public void Occupation(int num)
+    {
+        //탑에서 호출
+        occupation--;
+        pv.RPC("LineClose", RpcTarget.All,num);
+        CheckWinner();
+    }
+    [PunRPC]
+    private void LineClose(int n)
+    {
+        //n번째 라인 닫아주기 - 유닛 소환을 못하게 한다.
+        //몇번째 라인인지 어떻게 받아올까
+        //길을 나눠서 트리거로 해가지고 들어오면 되긴할꺼같네
+        lineBool[n] = false; //라인닫기
+        foreach (var obj in gameObjects[n])
+        {
+            AddressableManager.Instance.ReleaseObj(obj);
+        }
+        gameObjects[n].Clear();
+    }
+    private void CheckWinner()
+    {
+        if (occupation == 1)
+        {
+            pv.RPC("GameOver", RpcTarget.All);
+        }
+    }
+    [PunRPC]
+    private void GameOver()
+    {
+        isStart = false;
+        if (occupation == 1)
+        {
+            Debug.Log("승리");
+        }
+        else
+        {
+            Debug.Log("패배");
+        }
+    }
+
+    private void SetTime()
+    {
+        int startTime = PhotonNetwork.ServerTimestamp;
+        PhotonNetwork.CurrentRoom.SetCustomProperties(new ExitGames.Client.Photon.Hashtable { { "StartTime", startTime } });
+    }
+
+    IEnumerator Progress()
+    {
+        //시간의 흐름
+        //자원 올려주기
+        //점령
+        //승,패
+        
+        yield return null;
+
+    }
     private void Init()
     {
         //첫 시작
-        for(int i = 0; i < startHandCount; i++)
+        isStart = true;
+        for (int i = 0; i < startHandCount; i++)
         {
            DrawCard();
         }
+
+    }
+    private void Reset()
+    {
+        //Enable떄 init()을 해버려도 될
     }
     private async Task DrawCard()
     {
@@ -64,5 +161,22 @@ public class GameManager : MonoBehaviour
     public void RemoveCard(Unit u)
     {
         currentHands.Remove(u);
+    }
+    void Start()
+    {
+        GoToLobbyBtn.onClick.AddListener(() =>
+        {
+            NetworkManager.Instance.GotoLobby();
+        });
+        GoToRoomBtn.onClick.AddListener(() =>
+        {
+            NetworkManager.Instance.ReJoindRoom(PhotonNetwork.CurrentRoom.Name);
+        });
+        if (PhotonNetwork.IsMasterClient)
+        {
+            SetTime();
+        }
+        currentDeck = new Queue<Unit>(DataManager.Instance.SuffleDeck());
+        Init();
     }
 }
